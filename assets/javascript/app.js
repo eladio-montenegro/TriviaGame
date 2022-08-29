36793662b306e67f8f73086d3fbbcd86a0f0ae70
Eladio Montenegro <50347922+eladio-montenegro@users.noreply.github.com>
2022-08-29 13:31:51 -0500

    clean up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,85 +13,56 @@ $(document).ready(function(){
 	// TimeOut page after 1 minute
 	//setTimeout(timeUp, limit); <--- worked around it with a hack (count = 0 manaul set)
 
-
-
 	// Intialize the game with hidden Divs
 	$("#mid_game_container").hide();
 	$("#end_container").hide();
 
-	
-
 	// Set Scroll position so it looks good
 	// window.scrollTo(0, 500);
-
-
-
-
 	$("#start_button").on("click", function(){
 
 		// Hide the start Div from the user
 		$("#start_container").hide();
 
-
 		// Show the Game Div
 		$("#mid_game_container").show();
-
 		startCountdown();
 		return;
-
 	});
-
-
 
 	// Counts down and displays the time to the user
 	function countdown(){
-
 		// Decrement the counter, down from 60 seconds
 		count--;
-
 		// Display the count to the user in the DOM
     	$('#timer_number').html(count + " Seconds");
 
-    	
 
     	// ----------- Handle Cases for Time ar 0 Seconds -----------
 			// User finishes before time is up and clicks done
 			$("#done_button").on("click", function(){
-
 			// Stop the countdown and run the timeUp function
 			//clearInterval(startCountdown);
 			count = 0; // <---- Needed a hack since I couldn't get the clearInterval function to work... It's been a long week :/
 			return;
-
 			});
-
 
 			// Finish the game after the timer reaches 0
 			if(count == -1){
-
 				// Collect the radio inputs
 				timeUp();
-
 				// Hide the game Div from the user
 				$("#mid_game_container").hide();
-
 			}
-
-
 	}
-
 
 	// Show the countdown, increment is 1 second
 	function startCountdown(){
-
 		setInterval(countdown, 1000);
-
 	}
-
 
 	// Function to be run after the timer is up
 	function timeUp(){
-
 
 		// Checked values of Radio Buttons
 		var Q1 = $('input:radio[name="q1"]:checked').val();
@@ -106,7 +77,6 @@ $(document).ready(function(){
 		var Q10 = $('input:radio[name="q10"]:checked').val();
 
 
-
 		// Determine the right/wrong/unanswered counts ( This count be a lot more DRY :/ )
 		if(Q1 == undefined){
 			unansweredCount++;
@@ -118,7 +88,6 @@ $(document).ready(function(){
 			wrongCount++;
 		}
 
-
 		if(Q2 == undefined){
 			unansweredCount++;
 		}
@@ -128,7 +97,6 @@ $(document).ready(function(){
 		else{
 			wrongCount++;
 		}
-
 
 		if(Q3 == undefined){
 			unansweredCount++;
@@ -140,7 +108,6 @@ $(document).ready(function(){
 			wrongCount++;
 		}
 
-
 		if(Q4 == undefined){
 			unansweredCount++;
 		}
@@ -150,7 +117,6 @@ $(document).ready(function(){
 		else{
 			wrongCount++;
 		}
-
 
 		if(Q5 == undefined){
 			unansweredCount++;
@@ -162,7 +128,6 @@ $(document).ready(function(){
 			wrongCount++;
 		}
 
-
 		if(Q6 == undefined){
 			unansweredCount++;
 		}
@@ -172,7 +137,6 @@ $(document).ready(function(){
 		else{
 			wrongCount++;
 		}
-
 
 		if(Q7 == undefined){
 			unansweredCount++;
@@ -184,7 +148,6 @@ $(document).ready(function(){
 			wrongCount++;
 		}
 
-
 		if(Q8 == undefined){
 			unansweredCount++;
 		}
@@ -194,7 +157,6 @@ $(document).ready(function(){
 		else{
 			wrongCount++;
 		}
-
 
 		if(Q9 == undefined){
 			unansweredCount++;
@@ -206,8 +168,6 @@ $(document).ready(function(){
 			wrongCount++;
 		}
 
-
-
 		if(Q10 == undefined){
 			unansweredCount++;
 		}
@@ -218,22 +178,15 @@ $(document).ready(function(){
 			wrongCount++;
 		}
 
-
-
-
 		// After answers are validated, display the score results
 		$('#correct_answers').html(correctCount);
 		$('#wrong_answers').html(wrongCount);
 		$('#unanswered').html(unansweredCount);
 
-
 		// Show the completed Score Div
 		$("#end_container").show();
 
-
 		// Set Scroll position so it looks good
 		// window.scrollTo(0, 550);
-
 	}
-
 });
